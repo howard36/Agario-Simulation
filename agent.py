6102e2bbf0f020c93ae586a08bb66daa2e6f53dc
Plasma-Vortex <howardhalim@gmail.com>
2019-12-22 01:39:00 -0500

    basic foundations of game

--- agent.py
+++ agent.py
@@ -1,10 +1,13 @@
+import numpy
+
 class Agent:
     def __init__(self, input_agents, input_food):
         self.input_size = 4 + 3*input_agents + 2*input_food
         self.hidden_size = 50
         self.output_size = 2
-        self.param_size = (self.input_size + 1)*self.hidden_size
-                        + (self.hidden_size+1)*self.output_size
+        self.param_size = (self.input_size + 1)*self.hidden_size 
+                        + (self.hidden_size + 1)*self.output_size
+        self.params = 
 
     def move(self, pos, mass, radius, close_agents, close_food):
         inp = [1] # bias
